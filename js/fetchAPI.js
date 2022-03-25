3a94db8e5b4a3a493b652af12e64431276ebdb63
OsvaldoNegrellos14 <fabiannegrellos@gmail.com>
2022-03-24 22:33:26 -0600

    fix style for mobile device

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -17,3 +17,17 @@ const button = document.getElementById('btn-dice')
 button.addEventListener('click', getAdvideAPI)
 
 window.onload = getAdvideAPI()
+
+window.onresize = (event) => {
+  const widthScreen = event.target.screen.width
+  if (widthScreen <= 600) {
+    const src = document.getElementById('img-divider').src.split('/images/')
+    const newSrc = `${src[0]}/images/pattern-divider-mobile.svg`
+    document.getElementById('img-divider').src = newSrc
+    console.log(newSrc)
+  } else if (widthScreen >= 600) {
+    const src = document.getElementById('img-divider').src.split('/images/')
+    const newSrc = `${src[0]}/images/pattern-divider-desktop.svg`
+    document.getElementById('img-divider').src = newSrc
+  }
+}
